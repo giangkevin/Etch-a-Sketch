d769450bd9bdb795a33611579198d8e138664854
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-10-30 01:27:38 +0100

    Add slider range and button

--- javascript.js
+++ javascript.js
@@ -1,24 +1,46 @@
 function createGrid(size) {
+    reset();
     const container = document.querySelector(".container");
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
-        row.style.cssText = "display : flex; border : 1px solid black; flex : 1";
+        row.style.cssText = "display : flex; flex : 1";
         row.className = "row";
         container.appendChild(row);
         for (let j = 0; j < size; j++) {
             const column = document.createElement("div");
-            column.style.cssText = "display : flex; border : 1px solid black; flex : 1 ";
+            column.style.cssText = "display : flex; ; flex : 1 ";
             column.className = "column";
             row.appendChild(column);
         }
     }
+    const columns = document.querySelectorAll(".column");
+    columns.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        })
+    })
 }
 
-createGrid(5);
+function reset() {
+    const container = document.querySelector(".container");
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+}
 
-const columns = document.querySelectorAll(".column");
-columns.forEach((square) =>{
-    square.addEventListener('mouseover', () =>{
-        square.style.backgroundColor = "black";
-    })
+createGrid(16);
+
+
+const value = document.querySelector("#value");
+const input = document.querySelector("#size");
+
+value.textContent = input.value;
+
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value;
+})
+
+const button = document.querySelector(".reset")
+button.addEventListener('click', (event) => {
+    createGrid(value.textContent);
 })
