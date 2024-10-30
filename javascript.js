b9a0386fba29772fb44b342d7cca8b343b74cc63
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-10-30 18:04:51 +0100

    Add opacity slider and a color picker

--- javascript.js
+++ javascript.js
@@ -13,8 +13,8 @@ function createGrid(size) {
         for (let j = 0; j < size; j++) {
             const column = document.createElement("div");
             column.style.cssText = "display : flex; ; flex : 1 ";
-            column.dataset.opacity = 0.1; // Initial opacity close to 0
-            column.style.opacity = column.dataset.opacity;
+            // column.dataset.opacity = 0.1; // Initial opacity close to 0
+            // column.style.opacity = column.dataset.opacity;
             column.className = "column";
             row.appendChild(column);
         }
@@ -22,13 +22,24 @@ function createGrid(size) {
     const columns = document.querySelectorAll(".column");
     columns.forEach((square) => {
         square.addEventListener('mouseover', () => {
-            // Increasing the opacity by 10 %
-            let opacity = parseFloat(square.dataset.opacity) + 0.1;
-            if (opacity > 1) opacity = 1; 
+            // // Increasing the opacity by 10 %
+            // let opacity = parseFloat(square.dataset.opacity) + 0.1;
+            // if (opacity > 1) opacity = 1; 
             
-            square.dataset.opacity = opacity;
-            square.style.backgroundColor = getRandomColor();
-            square.style.opacity = opacity;
+            const color = document.querySelector("#palette")
+            let colorPicker = color.value;
+            color.addEventListener("input", (event) => {
+                colorPicker = target.event.value;
+            })
+
+            const opacity = document.querySelector("#opacity");
+            let opacitySelector = opacity.value;
+            
+            opacity.addEventListener("input", (event) => {
+                opacitySelector = target.event.value;
+            })
+            square.style.backgroundColor = colorPicker;
+            square.style.opacity = opacitySelector;
         })
     })
 }
@@ -55,19 +66,28 @@ function getRandomColor() {
 createGrid(16);
 
 
-const value = document.querySelector("#value");
-const input = document.querySelector("#size");
+const valueSize = document.querySelector("#valueSize");
+const inputSize = document.querySelector("#size");
 
-value.textContent = input.value;
+valueSize.textContent = inputSize.value;
 
 // Updating the label with the current number of the slider
-input.addEventListener("input", (event) => {
-    value.textContent = event.target.value;
+inputSize.addEventListener("input", (event) => {
+    valueSize.textContent = event.target.value;
+})
+
+const valueOpacity = document.querySelector("#valueOpacity");
+const inputOpacity = document.querySelector("#opacity");
+
+valueOpacity.textContent = inputOpacity.value;
+
+inputOpacity.addEventListener("input", (event) => {
+    valueOpacity.textContent = event.target.value;
 })
 
 // Event handler when the button is clicked, a new grid will be created with the value of the slider
 const button = document.querySelector(".reset")
 button.addEventListener('click', (event) => {
-    createGrid(value.textContent);
+    createGrid(valueSize.textContent);
 })
 
